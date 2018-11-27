a77c6d5571a496e29a04103fa48928fabda08069
T.Furuya <83071189+hydrangeafox@users.noreply.github.com>
2018-11-27 20:17:30 +0900

    Implemented pseudo graceful-shutdown capability

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,3 +1,15 @@
 import App
+import Vapor
 
-try app(.detect()).run()
+let a = try app(.detect())
+try a.asyncRun().flatMap { () -> Future<Void> in
+  let console = try a.make(Console.self)
+  if console.confirm("When you need graceful quit, answer yes!") {
+    print("Shutting down...")
+    try a.runningServer?.close().wait()
+  }
+  try a.runningServer?.onClose.then { () -> Future<Void> in
+    return .done(on:a)
+  }.wait()
+  return .done(on:a)
+}.wait()
