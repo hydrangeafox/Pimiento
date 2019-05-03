52aeefe166c8f7b904a58bf733a0cc873db61067
T.Furuya <83071189+hydrangeafox@users.noreply.github.com>
2019-05-03 19:30:10 +0900

    Backed out of the commit:a77c6d55 for normal shutdown sequence

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,15 +1,3 @@
 import App
-import Vapor
 
-let a = try app(.detect())
-try a.asyncRun().flatMap { () -> Future<Void> in
-  let console = try a.make(Console.self)
-  if console.confirm("When you need graceful quit, answer yes!") {
-    print("Shutting down...")
-    try a.runningServer?.close().wait()
-  }
-  try a.runningServer?.onClose.then { () -> Future<Void> in
-    return .done(on:a)
-  }.wait()
-  return .done(on:a)
-}.wait()
+try app(.detect()).run()
