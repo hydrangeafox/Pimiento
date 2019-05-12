c5343f47ab123d5c0700dca6f875ac35d0bbd941
T.Furuya <83071189+hydrangeafox@users.noreply.github.com>
2019-05-12 18:57:07 +0900

    [Bugfix] `File.write(on:)` blocks the event loop

--- Sources/App/File+Repository.swift
+++ Sources/App/File+Repository.swift
@@ -2,9 +2,15 @@ import Core
 
 extension File {
   func write(on worker:Worker) -> Future<File> {
-    return Future.map(on:worker) {
-      try self.data.write()
-      return self
-    }
+    let promise = worker.eventLoop.newPromise(File.self)
+    DispatchQueue.global().async(execute:DispatchWorkItem(qos:.default) {
+      do {
+        try self.data.write()
+        promise.succeed(result:self)
+      } catch {
+        promise.fail(error:error)
+      }
+    })
+    return promise.futureResult
   }
 }
