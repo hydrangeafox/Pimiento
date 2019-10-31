d96f60af0dd7d5b536191de581d77f961c873ddc
T.Furuya <83071189+hydrangeafox@users.noreply.github.com>
2019-10-31 19:47:46 +0900

    [Refactor] Reordered the declaration of methods on `LinkableCollection`

--- Sources/App/Controllers/LinkableCollection.swift
+++ Sources/App/Controllers/LinkableCollection.swift
@@ -8,6 +8,14 @@ final class LinkableCollection<T>: RouteCollection where T:ModifiablePivot,
     T.Database:JoinSupporting,
     T.Left.Database==T.Database, T.Right.Database==T.Database {
   func boot(router:Router) {
+    router.on(.OPTIONS, at:T.Right.parameter) { req -> Future<Bool> in
+      // TODO: This route exists just for our testing!
+      let binder = try req.parameters.next(T.Left.self)
+      let entity = try req.parameters.next(T.Right.self)
+      return flatMap(to:Bool.self, binder,entity) {
+        $0.siblings(through:T.self).isAttached($1, on:req)
+      }
+    }
     router.post(T.Right.parameter) { req -> Future<HTTPStatus> in
       // FIXME: Register this route with access control middlewares!
       let binder = try req.parameters.next(T.Left.self)
@@ -15,14 +23,6 @@ final class LinkableCollection<T>: RouteCollection where T:ModifiablePivot,
       return flatMap(to:HTTPStatus.self, binder,entity) {
         $0.siblings(through:T.self)
           .attach($1, on:req).transform(to:.created)
-      }
-    }
-    router.on(.OPTIONS, at:T.Right.parameter) { req -> Future<Bool> in
-      // TODO: This route exists just for our testing!
-      let binder = try req.parameters.next(T.Left.self)
-      let entity = try req.parameters.next(T.Right.self)
-      return flatMap(to:Bool.self, binder,entity) {
-        $0.siblings(through:T.self).isAttached($1, on:req)
       }
     }
     router.delete(T.Right.parameter) { req -> Future<HTTPStatus> in
