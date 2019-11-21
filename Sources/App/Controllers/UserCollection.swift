61550c1520ffc3fd41eeb55e779b2dbabfe51ed4
T.Furuya <83071189+hydrangeafox@users.noreply.github.com>
2019-11-21 20:24:36 +0900

    Added the `GET /` route in `UserCollection`

--- Sources/App/Controllers/UserCollection.swift
+++ Sources/App/Controllers/UserCollection.swift
@@ -2,6 +2,11 @@ import Vapor
 
 final class UserCollection: RouteCollection {
   func boot(router:Router) {
+    router.get() { req -> Future<[User.RenderableContent]> in
+      User.query(on:req).sort(User.idKey).all().map {
+        $0.map { user in user.renderable }
+      }
+    }
     router.put(UserManifest.self) { req,manifest -> Future<HTTPStatus> in
       let user = try req.requireAuthenticated(User.self)
       return try user.replace(with:manifest)
