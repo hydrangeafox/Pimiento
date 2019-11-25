717c6dd74f53b7f996b281a4484a7313feca557e
T.Furuya <83071189+hydrangeafox@users.noreply.github.com>
2019-11-25 19:45:31 +0900

    Increased `NIOServerConfig.maxBodySize` to accept higher quality photos

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,6 +4,9 @@ import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+    /// Accept uploading higher quality photos
+    services.register(NIOServerConfig.default(maxBodySize:5_000_000))
+
     /// Register providers first
     try services.register(FluentSQLiteProvider())
     try services.register(AuthenticationProvider())
